33e7d0039786c4b9de9bf381339ccba073b27264
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-05-31 19:18:54 +0900

    코드 커스텀마이징으로 입력폼만들기

---
+++ 실전편/Chapter_02/Custom_inputForm/Custom_inputForm/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Custom_inputForm
+//
+//  Created by 이가영 on 2020/05/31.
+//  Copyright © 2020 이가영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var paramEmail: UITextField!
+    var paramUpdate: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.navigationItem.title = "설정"
+        
+        let lblEmail = UILabel()
+        lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
+        lblEmail.text = "이메일"
+        
+        lblEmail.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblEmail)
+        
+        let lblUpdate = UILabel()
+        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
+        lblUpdate.text = "자동갱신"
+        lblUpdate.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblUpdate)
+        
+        let lblInterval = UILabel()
+        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
+        lblInterval.text = "갱신주기"
+        lblInterval.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblInterval)
+        
+        self.paramEmail = UITextField()
+        self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
+        self.paramEmail.font = UIFont.systemFont(ofSize: 13)
+        self.paramEmail.borderStyle = UITextField.BorderStyle.roundedRect
+        self.paramEmail.placeholder = "예) 1234@naver.com"
+        
+        self.view.addSubview(paramEmail)
+        
+        self.paramUpdate = UISwitch()
+        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
+        self.paramUpdate.setOn(true, animated: true)
+        
+        self.view.addSubview(paramUpdate)
+    }
+
+
+}
+
