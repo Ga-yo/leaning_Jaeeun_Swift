ecbe9d40eb3e96e21a8723f601fba1c8a98153a4
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-06-01 19:23:08 +0900

    커스텀코드로 입력폼 구현 완료!

---
+++ 실전편/Chapter_02/Custom_inputForm/Custom_inputForm/ReadViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ReadViewController.swift
+//  Custom_inputForm
+//
+//  Created by 이가영 on 2020/06/01.
+//  Copyright © 2020 이가영. All rights reserved.
+//
+
+import UIKit
+
+class ReadViewController: UIViewController {
+
+    var pEmail: String?
+    var pUpdate: Bool?
+    var pInterval: Double?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let email = UILabel()
+        let update = UILabel()
+        let interval = UILabel()
+        
+        email.frame = CGRect(x: 50, y: 100, width: 300, height: 30)
+        update.frame = CGRect(x: 50, y: 150, width: 300, height: 30)
+        interval.frame = CGRect(x: 50, y: 200, width: 300, height: 30)
+        
+        email.text = "전달받은 이메일 : \(self.pEmail!)"
+        update.text = "업데이트 여부 : \(self.pUpdate == true ? "업데이트 완료" : "업데이트 안 함")"
+        interval.text = "업데이트 주기 : \(self.pInterval!)"
+        
+        self.view.addSubview(email)
+        self.view.addSubview(update)
+        self.view.addSubview(interval)
+        
+        self.view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 실전편/Chapter_02/Custom_inputForm/Custom_inputForm/ViewController.swift
+++ 실전편/Chapter_02/Custom_inputForm/Custom_inputForm/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     var paramEmail: UITextField!
     var paramUpdate: UISwitch!
+    var paramInterval: UIStepper!
+    var textUpdate: UILabel!
+    var textInterval: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,7 @@ class ViewController: UIViewController {
         self.navigationItem.title = "설정"
         
         let lblEmail = UILabel()
+        
         lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
         lblEmail.text = "이메일"
         
@@ -27,6 +31,7 @@ class ViewController: UIViewController {
         self.view.addSubview(lblEmail)
         
         let lblUpdate = UILabel()
+        
         lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
         lblUpdate.text = "자동갱신"
         lblUpdate.font = UIFont.systemFont(ofSize: 14)
@@ -34,6 +39,7 @@ class ViewController: UIViewController {
         self.view.addSubview(lblUpdate)
         
         let lblInterval = UILabel()
+        
         lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
         lblInterval.text = "갱신주기"
         lblInterval.font = UIFont.systemFont(ofSize: 14)
@@ -41,20 +47,72 @@ class ViewController: UIViewController {
         self.view.addSubview(lblInterval)
         
         self.paramEmail = UITextField()
+        
         self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
         self.paramEmail.font = UIFont.systemFont(ofSize: 13)
         self.paramEmail.borderStyle = UITextField.BorderStyle.roundedRect
         self.paramEmail.placeholder = "예) 1234@naver.com"
-        
+        self.paramEmail.autocapitalizationType = .none
         self.view.addSubview(paramEmail)
         
         self.paramUpdate = UISwitch()
+        
         self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
         self.paramUpdate.setOn(true, animated: true)
         
         self.view.addSubview(paramUpdate)
+        
+        self.paramInterval = UIStepper()
+        
+        self.paramInterval.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramInterval.minimumValue = 0 //스테퍼가 가질 수 있는 최소값
+        self.paramInterval.maximumValue = 100 //스테퍼가 가질 수 있는 최대값
+        self.paramInterval.stepValue = 1 //스테퍼의 값 변경 단위
+        self.paramInterval.value = 0 //초기값
+        
+        self.view.addSubview(self.paramInterval)
+        
+        self.textUpdate = UILabel()
+        
+        self.textUpdate.frame = CGRect(x: 250, y: 150, width: 100, height: 30)
+        self.textUpdate.font = UIFont.systemFont(ofSize: 12)
+        self.textUpdate.textColor = UIColor.red
+        self.textUpdate.text = "갱신함"
+        
+        self.view.addSubview(textUpdate)
+        
+        self.textInterval = UILabel()
+        
+        self.textInterval.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
+        self.textInterval.font = UIFont.systemFont(ofSize: 12)
+        self.textInterval.textColor = UIColor.red
+        self.textInterval.text = "0분마다"
+        
+        self.view.addSubview(self.textInterval)
+        
+        self.paramUpdate.addTarget(self, action: #selector(presentUpdateValue(_:)), for: .valueChanged)
+        self.paramInterval.addTarget(self, action: #selector(presentIntervalValue(_:)), for: .valueChanged)
+        
+        let submitBtn = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(submit(_:)))
+        
+        self.navigationItem.rightBarButtonItem = submitBtn
     }
 
+    @objc func presentUpdateValue(_ sender: UISwitch){
+        self.textUpdate.text = (sender.isOn == true ? "갱신함" : "갱신하지 않음")
+    }
 
+    @objc func presentIntervalValue(_ sender: UIStepper){
+        self.textInterval.text = ("\(Int(sender.value))분마다")
+    }
+    
+    @objc func submit(_ sender: Any){
+        let rvc = ReadViewController()
+        rvc.pEmail = self.paramEmail.text
+        rvc.pUpdate = self.paramUpdate.isOn
+        rvc.pInterval = self.paramInterval.value
+        
+        self.navigationController?.pushViewController(rvc, animated: true)
+    }
 }
 
